c13a8df2ac32b2032768f9e12f6efece6aa9602e
MarKoz721 <65984906+MarKoz721@users.noreply.github.com>
2020-05-27 01:13:48 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="utilities",
+    version='0.3.1',
+    description="Utilities for machine learning.",
+    long_description="",
+    classifiers=[],
+    author="Martin Kozlovsky",
+    packages=["own_utilities"],
+    package_dir={"own_utilities": "."},
+    setup_requires=["setuptools_scm"],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        "sklearn"
+    ]
+)
